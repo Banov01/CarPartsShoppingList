6e967374e630628f5f3c6a00b7389c8c37c4bd3b
Georgi <87902608+Banov01@users.noreply.github.com>
2022-03-02 09:26:03 +0200

    Initial commit

---
+++ CarPartsShoppingList/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CarPartsShoppingList.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
