f1ccc3d6925d810693be3769a8af57bc5d983e84
Georgi Banov <georgibanov2001@gmail.com>
2022-03-10 16:47:29 +0200

    New Migrations For Models

---
+++ CarPartsShoppingList.Infrastructure/Migrations/20220310144659_ModelsMovedToData.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarPartsShoppingList.Infrastructure.Migrations
+{
+    public partial class ModelsMovedToData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
