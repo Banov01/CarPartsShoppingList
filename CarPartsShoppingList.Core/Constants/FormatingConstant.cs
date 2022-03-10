63dd666474f252725d0feef8025c0d7db2910277
Georgi Banov <georgibanov2001@gmail.com>
2022-03-10 16:07:07 +0200

    Fixed problem with _UserMessages

--- CarPartsShoppingList.Core/Constants/FormatingConstant.cs
+++ CarPartsShoppingList.Core/Constants/FormatingConstant.cs
@@ -1,4 +1,10 @@
-﻿namespace Warehouse.Core.Constants
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warehouse.Core.Constants
 {
     public class FormatingConstant
     {
