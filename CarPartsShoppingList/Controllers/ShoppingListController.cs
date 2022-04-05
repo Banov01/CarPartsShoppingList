5355ddd1baebb73283ab9a07adc47b86edc62197
Georgi Banov <georgibanov2001@gmail.com>
2022-04-05 16:51:24 +0300

    More fixes on index of engine, suspension and transmision

--- CarPartsShoppingList/Controllers/ShoppingListController.cs
+++ CarPartsShoppingList/Controllers/ShoppingListController.cs
@@ -1,12 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarPartsShoppingList.Core.Contracts;
+using CarPartsShoppingList.Core.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CarPartsShoppingList.Controllers
 {
     public class ShoppingListController : Controller
     {
+        private readonly IShoppingListService shoppingListService;
+
+        public ShoppingListController(IShoppingListService shoppingListService)
+        {
+            this.shoppingListService = shoppingListService;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
+
+        public IActionResult AddShoppingList()
+        {
+            var viewModel = new ShoppingListViewModel();
+
+            return View("Edit", viewModel);
+        }
+
+        public IActionResult EditShoppingList(int id)
+        {
+            var model = shoppingListService.GetShoppingList(id);
+
+            return View("Edit", model);
+        }
+
+
     }
 }
