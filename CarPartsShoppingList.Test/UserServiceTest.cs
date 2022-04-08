def86de8bca99db857f63c869930e5f87882d62a
Georgi Banov <georgibanov2001@gmail.com>
2022-04-08 11:49:35 +0300

    Added some tests for user service

--- CarPartsShoppingList.Test/UserServiceTest.cs
+++ CarPartsShoppingList.Test/UserServiceTest.cs
@@ -1,3 +1,6 @@
+using CarPartsShoppingList.Core.Services;
+using CarPartsShoppingList.Infrastructure.Data.Repositories;
+using Moq;
 using NUnit.Framework;
 
 namespace CarPartsShoppingList.Test
@@ -5,15 +8,28 @@ namespace CarPartsShoppingList.Test
     [TestFixture]
     public class UserServiceTest
     {
+        private UserService userService;
+        private Mock<IApplicatioDbRepository> repo;
         [SetUp]
         public void Setup()
         {
+            var repo = new Mock<IApplicatioDbRepository>();
+
+            userService = new UserService(repo.Object);
         }
 
         [Test]
-        public void Test1()
+        public void Get_User_By_Id_Is_Not_NUll()
         {
-            Assert.Pass();
+            var result = userService.GetUserById("");
+            Assert.That(result, Is.Not.Null, "User id is null.");
+        }
+
+        [Test]
+        public void Get_User_By_Id_Is_Complete()
+        {
+            var result = userService.GetUserById("");
+            Assert.That(result.IsCompleted, "Get user by id is'n completed.");
         }
     }
 }
